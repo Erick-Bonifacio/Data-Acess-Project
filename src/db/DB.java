8dc164edbb01ddd822725642a40c5618664088d9
Erick de Alvarenga Bonifacio <erickalvarengabonifacio@hotmail.com>
2024-02-29 19:21:29 -0300

    Create java project

---
+++ src/db/DB.java
@@ -0,0 +1,50 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DB {
+
+    public static Connection conn = null;
+
+
+    public static Connection getConnection(){
+        try {
+            if(conn == null){
+                //conn = DriverManager.getConnection(url, user, password);
+                
+                Properties props = loadProperties();
+                String url = props.getProperty("dburl");
+                conn = DriverManager.getConnection(url, props);
+                return conn;
+            }
+            else return conn;
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+
+    public static void closeConection(){
+        try{
+            if(conn == null) conn.close();
+        }
+        catch(SQLException e) {
+            throw new DbException(e.getMessage());
+        }
+    }
+
+    private static Properties loadProperties(){
+        try(FileInputStream fs = new FileInputStream("db.properties")){
+            Properties props = new Properties();
+            props.load(fs);
+            return props;
+        }
+        catch(IOException e){
+            throw new DbException(e.getMessage());
+        }
+    }
+    
+}
