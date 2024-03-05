be601e945dff9aca7cea3f69ada52b465fba0aee
Erick de Alvarenga Bonifacio <erickalvarengabonifacio@hotmail.com>
2024-03-05 13:45:30 -0300

    Seller Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,12 @@
+import java.util.Date;
+
 public class Program {
     
     public static void main(String[] args){
 
         Department obj = new Department(1, "Books");
+        Seller seller = new Seller(21, "bob", "Bob@gmail.com", new Date(), 3000.00, obj);
 
-        System.out.println(obj);
+        System.out.println(seller);
     }
 }
