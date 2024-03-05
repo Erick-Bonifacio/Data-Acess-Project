d21bef91438785aa2269dec5ac3ee56e251ffe5d
Erick de Alvarenga Bonifacio <erickalvarengabonifacio@hotmail.com>
2024-03-05 13:53:50 -0300

    DepartmentDAO and SellerDAO interfaces

---
+++ src/modelDAO/SellerDao.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public interface SellerDao {
+    
+    void insert(Seller obj);
+    void update(Seller obj);
+    void deleteById(Integer id);
+    Department findById(Integer id);
+    List<Seller> findAll();
+}
